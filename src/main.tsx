7527c5fc2bacb2ed8bc9271df181d33cd62409f2
zhangkejia <18771964684@163.com>
2023-07-15 22:43:12 +0800

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ErrorPage } from './components/ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Outlet></Outlet>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        index: true,
+        element: <div>空</div>,
+      },
+      {
+        path: 'welcome',
+        element: <Outlet></Outlet>,
+        children: [
+          {
+            path: '1',
+            element: <div>1<NavLink to="/welcome/2">下一页</NavLink></div>,
+          },
+          {
+            path: '2',
+            element: <div>2<NavLink to="/welcome/3">下一页</NavLink></div>,
+          },
+          {
+            path: '3',
+            element: <div>4<NavLink to="/welcome/xxxx">下一页</NavLink></div>,
+          },
+        ],
+      },
+
+    ],
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
